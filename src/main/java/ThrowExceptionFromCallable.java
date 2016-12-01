f3300b7848571ac8773a6cf49e66408c728436ac
saoprb <saoprb@gmail.com>
2016-12-01 15:00:48 +0800

    Refactor. Change task result Integer to a proper class result.

--- src/main/java/ThrowExceptionFromCallable.java
+++ src/main/java/ThrowExceptionFromCallable.java
@@ -8,45 +8,53 @@ import java.util.concurrent.*;
  */
 public class ThrowExceptionFromCallable {
 
-    private static class ThrowTask implements Callable<Integer> {
+    private final static class TaskResult {
+        private final String threadName;
+        private final String className;
+        private final Integer result;
 
-        private Integer status = null;
+        public TaskResult(String threadName, String className, Integer result) {
+            this.threadName = threadName;
+            this.className = className;
+            this.result = result;
+        }
 
-        @Override
-        public Integer call() throws Exception {
-            Thread.sleep(new Random().nextInt(5*1000));
-            System.out.format("ThrowTask: %s%n", Thread.currentThread().getName());
-            int divideByZero = 1/0;
-            status = 0;
-            return status;
+        public String toString() {
+            return String.format("ThreadName: %s, ClassName: %s, Result: %s", threadName, className, result);
         }
     }
 
-    private static class OkTask implements Callable<Integer> {
-
-        private Integer status = null;
-
+    private static class ThrowTask implements Callable<TaskResult> {
         @Override
-        public Integer call() throws Exception {
+        public TaskResult call() throws Exception {
+            Thread.sleep(new Random().nextInt(5*1000));
+            System.out.format("ThrowTask: %s%n", Thread.currentThread().getName());
+            int divideByZero = 1/0;
+            return new TaskResult(Thread.currentThread().getName(), this.getClass().getName(), 0);
+        }
+    }
+
+    private static class OkTask implements Callable<TaskResult> {
+        @Override
+        public TaskResult call() throws Exception {
             Thread.sleep(new Random().nextInt(5*1000));
             System.out.format("OkTask: %s%n", Thread.currentThread().getName());
-            status = 0;
-            return status;
+            return new TaskResult(Thread.currentThread().getName(), this.getClass().getName(), 0);
         }
     }
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(5);
-        Callable<Integer> throwTask = new ThrowTask();
-        Callable<Integer> okTask = new OkTask();
-        List<Future<Integer>> futures = new ArrayList<>();
+        Callable<TaskResult> throwTask = new ThrowTask();
+        Callable<TaskResult> okTask = new OkTask();
+        List<Future<TaskResult>> futures = new ArrayList<>();
 
         futures.add(executorService.submit(okTask));
         futures.add(executorService.submit(throwTask));
 
-        for (Future<Integer> aFuture : futures) {
+        for (Future<TaskResult> aFuture : futures) {
             try {
-                System.out.format("OkFuture: %s%n", aFuture.get());
+                System.out.format("Task Result: %s%n", aFuture.get().toString());
             } catch (InterruptedException | ExecutionException ex) {
                 ex.printStackTrace();
             }
